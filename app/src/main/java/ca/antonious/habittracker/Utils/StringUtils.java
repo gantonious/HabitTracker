9caef7b77f45527b35dc8f95dacbef4c8649c9e3
George Antonious <antoniou@ualberta.ca>
2016-09-02 18:51:21 -0600

    Added String utils

---
+++ app/src/main/java/ca/antonious/habittracker/Utils/StringUtils.java
@@ -0,0 +1,10 @@
+package ca.antonious.habittracker.Utils;
+
+/**
+ * Created by George on 2016-09-02.
+ */
+public class StringUtils {
+    public static boolean isStringNullOrEmpty(String string) {
+        return string == null || string.isEmpty();
+    }
+}
