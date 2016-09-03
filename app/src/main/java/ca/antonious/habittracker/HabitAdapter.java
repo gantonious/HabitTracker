3e615d7e364e26f9cbf6671100232ec366bec6fb
George Antonious <antoniou@ualberta.ca>
2016-09-02 19:05:15 -0600

    Added RecyclerView to HabitListActivity and created adapter for it

---
+++ app/src/main/java/ca/antonious/habittracker/HabitAdapter.java
@@ -0,0 +1,64 @@
+package ca.antonious.habittracker;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ca.antonious.habittracker.models.Habit;
+
+/**
+ * Created by George on 2016-09-01.
+ */
+public class HabitAdapter extends RecyclerView.Adapter<HabitAdapter.ViewHolder> {
+    private List<Habit> habits = new ArrayList<>();
+
+    public void setHabits(List<? extends Habit> habits) {
+        this.habits.clear();
+        this.habits.addAll(habits);
+    }
+
+    @Override
+    public HabitAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.habit_list_item, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(HabitAdapter.ViewHolder holder, int position) {
+        Habit habit = habits.get(position);
+
+        holder.setTitle(habit.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return habits.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView title;
+
+        public ViewHolder(View view) {
+            super(view);
+
+            title = (TextView) view.findViewById(R.id.habit_title);
+
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+        }
+
+        public void setTitle(String title) {
+            this.title.setText(title);
+        }
+    }
+}
--- app/src/main/java/ca/antonious/habittracker/habitlist/HabitListActivity.java
+++ app/src/main/java/ca/antonious/habittracker/habitlist/HabitListActivity.java
@@ -1,17 +1,24 @@
 package ca.antonious.habittracker.habitlist;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import ca.antonious.habittracker.HabitAdapter;
 import ca.antonious.habittracker.R;
 
 public class HabitListActivity extends AppCompatActivity {
+    private RecyclerView habitRecyclerView;
+    private HabitAdapter habitAdapter = new HabitAdapter();
+
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,31 +27,34 @@ public class HabitListActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        habitRecyclerView = (RecyclerView) findViewById(R.id.habit_recycler_view);
+        habitRecyclerView.setAdapter(habitAdapter);
+        habitRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+        habitRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy){
+                if (dy > 10) {
+                    fab.hide();
+                } else if (dy < 0) {
+                    fab.show();
+                }
             }
         });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
