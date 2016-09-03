5eeb891e34e83bb35b5cfc986e05ac505dd417cb
George Antonious <antoniou@ualberta.ca>
2016-09-02 19:59:57 -0600

    Id habits to allow habits with the same name

--- app/src/main/java/ca/antonious/habittracker/models/Habit.java
+++ app/src/main/java/ca/antonious/habittracker/models/Habit.java
@@ -2,13 +2,27 @@ package ca.antonious.habittracker.models;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by George on 2016-09-01.
  */
 public class Habit {
+    private String id;
     private String name;
     private List<HabitCompletion> completions = new ArrayList<>();
+
+    public Habit() {
+        setId(UUID.randomUUID().toString());
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
