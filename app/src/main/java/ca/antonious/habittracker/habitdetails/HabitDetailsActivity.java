d390fce00ab284e977f5e20bd10e6f4c0edea28c
George Antonious <antoniou@ualberta.ca>
2016-09-02 21:28:55 -0600

    Added habit details activity

---
+++ app/src/main/java/ca/antonious/habittracker/habitdetails/HabitDetailsActivity.java
@@ -0,0 +1,15 @@
+package ca.antonious.habittracker.habitdetails;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ca.antonious.habittracker.R;
+
+public class HabitDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_habit_details);
+    }
+}
