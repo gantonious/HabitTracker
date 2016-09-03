7aca8ae6387c97bcbf8a6418807c20fcb6b7ee40
George Antonious <antoniou@ualberta.ca>
2016-09-03 16:53:19 -0600

    Added ability to delete habit from details screen

--- app/src/main/java/ca/antonious/habittracker/habitdetails/HabitDetailsActivity.java
+++ app/src/main/java/ca/antonious/habittracker/habitdetails/HabitDetailsActivity.java
@@ -1,17 +1,18 @@
 package ca.antonious.habittracker.habitdetails;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
-
-import java.util.Date;
 
 import ca.antonious.habittracker.BaseActivity;
 import ca.antonious.habittracker.HabitRepository;
 import ca.antonious.habittracker.R;
 import ca.antonious.habittracker.models.Habit;
-import ca.antonious.habittracker.models.HabitCompletion;
 
 public class HabitDetailsActivity extends BaseActivity {
     private TextView titleTextView;
@@ -56,5 +57,47 @@ public class HabitDetailsActivity extends BaseActivity {
         habitCompletionAdapter.clear();
         habitCompletionAdapter.addAll(habit.getCompletions());
         habitCompletionAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.habit_details_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            onDelete();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void onDelete() {
+        createDeleteAlertDialog().show();
+    }
+
+    private AlertDialog createDeleteAlertDialog() {
+        return new AlertDialog.Builder(this)
+                .setTitle("Confirmation")
+                .setMessage("Are you sure you want to delete this habit?")
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        habitRepository.removeHabit(habitRepository.getHabits().get(0).getId());
+                        finish();
+                    }
+                })
+                .create();
     }
 }
