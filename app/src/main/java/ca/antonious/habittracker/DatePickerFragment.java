2ed3e64762ea51d9862258ebdc28d458aedeb933
George Antonious <antoniou@ualberta.ca>
2016-09-03 09:23:55 -0600

    Add date picker fragment

---
+++ app/src/main/java/ca/antonious/habittracker/DatePickerFragment.java
@@ -0,0 +1,29 @@
+package ca.antonious.habittracker;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+
+/**
+ * Created by George on 2016-09-03.
+ */
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+    }
+}
