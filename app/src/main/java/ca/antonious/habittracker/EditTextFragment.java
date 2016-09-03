0612b1029ddbbcf60a426a48501b6ab7d9c08ce6
George Antonious <antoniou@ualberta.ca>
2016-09-03 16:30:55 -0600

    Change alert dialog import so it uses theme

--- app/src/main/java/ca/antonious/habittracker/EditTextFragment.java
+++ app/src/main/java/ca/antonious/habittracker/EditTextFragment.java
@@ -1,32 +1,48 @@
 package ca.antonious.habittracker;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.support.v7.app.AlertDialog;
 import android.widget.EditText;
 
 /**
  * Created by George on 2016-09-03.
  */
 public class EditTextFragment extends DialogFragment {
+    private EditText editText;
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         super.onCreateDialog(savedInstanceState);
 
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
-        alertDialogBuilder.setTitle("Name");
-        alertDialogBuilder.setView(new EditText(getActivity()));
-        alertDialogBuilder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
 
-            }
-        });
+        alertDialogBuilder.setTitle("Name")
+                          .setView(new EditText(getActivity()))
+                          .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                              @Override
+                              public void onClick(DialogInterface dialog, int which) {
+
+                              }
+                          })
+                          .setPositiveButton("Set", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                          });
 
 
         return alertDialogBuilder.show();
+    }
+
+    private EditText ensureEditText() {
+        if (editText == null) {
+            editText = new EditText(getActivity());
+        }
+        return editText;
     }
 
 }
