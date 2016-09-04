ed3b5745535644adb0881982823d8084875fc305
George Antonious <antoniou@ualberta.ca>
2016-09-04 09:38:08 -0600

    Id habit completions

--- app/src/main/java/ca/antonious/habittracker/models/HabitCompletion.java
+++ app/src/main/java/ca/antonious/habittracker/models/HabitCompletion.java
@@ -1,15 +1,26 @@
 package ca.antonious.habittracker.models;
 
 import java.util.Date;
+import java.util.UUID;
 
 /**
  * Created by George on 2016-09-01.
  */
 public class HabitCompletion {
+    private String id;
     private Date completionTime;
 
     public HabitCompletion(Date completionTime) {
+        setId(UUID.randomUUID().toString());
         setCompletionTime(completionTime);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public Date getCompletionTime() {
