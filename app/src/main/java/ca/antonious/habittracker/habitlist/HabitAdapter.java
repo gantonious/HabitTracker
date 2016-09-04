de54e09458b726aee095feef1e81573c633aa2b8
George Antonious <antoniou@ualberta.ca>
2016-09-03 21:21:43 -0600

    Indicate if habit has been recently completed in timeline

--- app/src/main/java/ca/antonious/habittracker/habitlist/HabitAdapter.java
+++ app/src/main/java/ca/antonious/habittracker/habitlist/HabitAdapter.java
@@ -7,10 +7,14 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.TextView;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import ca.antonious.habittracker.ArrayAdapter;
 import ca.antonious.habittracker.BaseViewHolder;
 import ca.antonious.habittracker.R;
 import ca.antonious.habittracker.models.Habit;
+import ca.antonious.habittracker.models.HabitCompletion;
 
 /**
  * Created by George on 2016-09-01.
@@ -34,8 +38,36 @@ public class HabitAdapter extends ArrayAdapter<Habit, HabitAdapter.ViewHolder> {
         super.onBindViewHolder(holder, position);
 
         Habit habit = get(position);
+
+        if (hasHabitBeenCompletedRecently(habit)) {
+            holder.setCompleted();
+        } else {
+            holder.setNotCompleted();
+        }
+
         holder.setTitle(habit.getName());
         holder.setOnCheckListener(handleCheck(habit));
+    }
+
+    private boolean hasHabitBeenCompletedRecently(Habit habit) {
+        boolean hasBeenCompletedRecently = false;
+
+        for (HabitCompletion habitCompletion: habit.getCompletions()) {
+
+            if (isDateWithinADay(habitCompletion.getCompletionTime())) {
+                hasBeenCompletedRecently = true;
+                break;
+            }
+        }
+
+        return hasBeenCompletedRecently;
+    }
+
+    private boolean isDateWithinADay(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.DAY_OF_YEAR, calendar.get(Calendar.DAY_OF_YEAR) - 1);
+
+        return date.compareTo(calendar.getTime()) > 0;
     }
 
     private CheckBox.OnCheckedChangeListener handleCheck(final Habit habit) {
@@ -68,6 +100,14 @@ public class HabitAdapter extends ArrayAdapter<Habit, HabitAdapter.ViewHolder> {
 
         public void setOnCheckListener(CheckBox.OnCheckedChangeListener onCheckListener) {
             checkBox.setOnCheckedChangeListener(onCheckListener);
+        }
+
+        public void setCompleted() {
+            checkBox.setChecked(true);
+        }
+
+        public void setNotCompleted() {
+            checkBox.setChecked(false);
         }
 
         @Override
