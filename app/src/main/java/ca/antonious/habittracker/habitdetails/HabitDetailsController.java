1bf6e17105ff3173a7af90dec1833c8d65ce4f42
George Antonious <antoniou@ualberta.ca>
2016-09-04 10:04:02 -0600

    Expose methods to load/manipulate habit in the HabitDetailsController

--- app/src/main/java/ca/antonious/habittracker/habitdetails/HabitDetailsController.java
+++ app/src/main/java/ca/antonious/habittracker/habitdetails/HabitDetailsController.java
@@ -1,12 +1,43 @@
 package ca.antonious.habittracker.habitdetails;
 
+import ca.antonious.habittracker.CompleteHabit;
 import ca.antonious.habittracker.IController;
+import ca.antonious.habittracker.IHabitRepository;
+import ca.antonious.habittracker.RemoveCompletion;
+import ca.antonious.habittracker.models.Habit;
 
 /**
  * Created by George on 2016-09-04.
  */
 public class HabitDetailsController implements IController<IHabitDetailsView> {
+    private Habit habit;
     private IHabitDetailsView habitDetailsView;
+    private IHabitRepository habitRepository;
+
+    public HabitDetailsController(IHabitRepository habitRepository) {
+        this.habitRepository = habitRepository;
+    }
+
+    public void loadHabit(String habitId) {
+        Habit habit = habitRepository.getHabit(habitId);
+
+        if (habit != null) {
+            this.habit = habit;
+            disptachDisplayHabit(habit);
+        }
+    }
+
+    public void markHabitAsComplete() {
+        if (habit != null) {
+            new CompleteHabit(habitRepository).complete(habit.getId());
+        }
+    }
+
+    public void removeHabitCompletion(String completionId) {
+        if (habit != null) {
+            new RemoveCompletion(habitRepository).removeCompletionFromHabit(habit, completionId);
+        }
+    }
 
     @Override
     public void attachView(IHabitDetailsView view) {
@@ -16,5 +47,11 @@ public class HabitDetailsController implements IController<IHabitDetailsView> {
     @Override
     public void detachView() {
         this.habitDetailsView = null;
+    }
+
+    private void disptachDisplayHabit(Habit habit) {
+        if (habitDetailsView != null) {
+            habitDetailsView.displayHabit(habit);
+        }
     }
 }
--- app/src/main/java/ca/antonious/habittracker/habitdetails/IHabitDetailsView.java
+++ app/src/main/java/ca/antonious/habittracker/habitdetails/IHabitDetailsView.java
@@ -1,8 +1,10 @@
 package ca.antonious.habittracker.habitdetails;
 
+import ca.antonious.habittracker.models.Habit;
+
 /**
  * Created by George on 2016-09-04.
  */
 public interface IHabitDetailsView {
-
+    void displayHabit(Habit habit);
 }
