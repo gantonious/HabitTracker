71d51b85aa82b2768cdd5c7f76d9464d032f3fc0
George Antonious <antoniou@ualberta.ca>
2016-09-06 18:50:56 -0600

    Render completion stats in details activity

--- app/src/main/java/ca/antonious/habittracker/habitdetails/HabitDetailsActivity.java
+++ app/src/main/java/ca/antonious/habittracker/habitdetails/HabitDetailsActivity.java
@@ -7,15 +7,22 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 import ca.antonious.habittracker.ArrayAdapter;
 import ca.antonious.habittracker.BaseActivity;
 import ca.antonious.habittracker.Constants;
 import ca.antonious.habittracker.DaysToDescriptionMapper;
 import ca.antonious.habittracker.R;
+import ca.antonious.habittracker.models.Days;
 import ca.antonious.habittracker.models.Habit;
 import ca.antonious.habittracker.models.HabitCompletion;
 
@@ -23,6 +30,9 @@ public class HabitDetailsActivity extends BaseActivity implements IHabitDetailsV
     private TextView titleTextView;
     private TextView creationDateTextView;
     private TextView habitDatesTextView;
+    private TextView habitCompletionStatsDescription;
+    private TextView habitMissedCompletionsDescription;
+    private Button completeHabitButton;
 
     private RecyclerView completionsRecyclerView;
     private LinearLayoutManager linearLayoutManager;
@@ -38,6 +48,7 @@ public class HabitDetailsActivity extends BaseActivity implements IHabitDetailsV
         bindViews();
         resolveDependencies();
         setUpRecyclerView();
+        handleCompleteButtonClicks();
     }
 
     private void bindViews() {
@@ -45,6 +56,9 @@ public class HabitDetailsActivity extends BaseActivity implements IHabitDetailsV
         creationDateTextView = (TextView) findViewById(R.id.habit_details_created_date);
         habitDatesTextView = (TextView) findViewById(R.id.habit_details_dates);
         completionsRecyclerView = (RecyclerView) findViewById(R.id.habit_details_recent_completions_list);
+        habitCompletionStatsDescription = (TextView) findViewById(R.id.habit_details_total_completions);
+        habitMissedCompletionsDescription = (TextView) findViewById(R.id.habit_details_total_missed_completions);
+        completeHabitButton = (Button) findViewById(R.id.habit_details_complete_button);
     }
 
     private void resolveDependencies() {
@@ -67,11 +81,22 @@ public class HabitDetailsActivity extends BaseActivity implements IHabitDetailsV
         });
     }
 
+    private void handleCompleteButtonClicks() {
+        completeHabitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                habitDetailsController.markHabitAsComplete();
+            }
+        });
+    }
+
     @Override
     public void displayHabit(Habit habit) {
         titleTextView.setText(habit.getName());
         habitDatesTextView.setText(new DaysToDescriptionMapper().map(habit.getDaysToComplete()));
         creationDateTextView.setText(getCreationDateDescription(habit));
+        habitCompletionStatsDescription.setText(getCompletionsDescription(habit));
+        habitMissedCompletionsDescription.setText(getMissedCompletionsDescription(habit));
 
         habitCompletionAdapter.clear();
         habitCompletionAdapter.addAll(habit.getCompletions());
@@ -81,6 +106,60 @@ public class HabitDetailsActivity extends BaseActivity implements IHabitDetailsV
     private String getCreationDateDescription(Habit habit) {
         SimpleDateFormat humanReadableDateFormat = new SimpleDateFormat("MMM dd, yyyy");
         return "Started on " + humanReadableDateFormat.format(habit.getStartDate());
+    }
+
+    private String getCompletionsDescription(Habit habit) {
+        if (habit.getCompletions().size() > 1) {
+            return String.format("Completed %d times", habit.getCompletions().size());
+        } else if (habit.getCompletions().size() == 1) {
+            return "Completed once";
+        }
+        return "Never completed";
+    }
+
+    private String getMissedCompletionsDescription(Habit habit) {
+        int daysMissed = getMissedDays(habit);
+
+        if (daysMissed > 1) {
+            return String.format("Missed %d times", daysMissed);
+        } else if (daysMissed == 1) {
+            return "Missed once";
+        }
+        return "Never missed";
+    }
+
+    private int getMissedDays(Habit habit) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_YEAR, -1);
+
+        Calendar startingDate = Calendar.getInstance();
+        startingDate.setTime(habit.getStartDate());
+
+        int total = 0;
+        List<Integer> calenderDays = mapHabitDaysToCalenderDays(habit);
+
+        while (calendar.compareTo(startingDate) > 0) {
+            if (calenderDays.contains(calendar.get(Calendar.DAY_OF_WEEK))) {
+                total++;
+            }
+            calendar.add(Calendar.DAY_OF_YEAR, -1);
+        }
+
+        return total;
+    }
+
+    private List<Integer> mapHabitDaysToCalenderDays(Habit habit) {
+        List<Integer> days = new ArrayList<>();
+
+        if (habit.getDaysToComplete().contains(Days.SUNDAY)) days.add(Calendar.SUNDAY);
+        if (habit.getDaysToComplete().contains(Days.MONDAY)) days.add(Calendar.MONDAY);
+        if (habit.getDaysToComplete().contains(Days.TUESDAY)) days.add(Calendar.TUESDAY);
+        if (habit.getDaysToComplete().contains(Days.WEDNESDAY)) days.add(Calendar.WEDNESDAY);
+        if (habit.getDaysToComplete().contains(Days.THURSDAY)) days.add(Calendar.THURSDAY);
+        if (habit.getDaysToComplete().contains(Days.FRIDAY)) days.add(Calendar.FRIDAY);
+        if (habit.getDaysToComplete().contains(Days.SATURDAY)) days.add(Calendar.SATURDAY);
+
+        return days;
     }
 
     private void onDelete() {
