49d76a97447beba380ea5dc269f11d0b4be40b8c
George Antonious <antoniou@ualberta.ca>
2016-09-15 11:54:42 -0600

    Added test case for completing a habit that does not exist

--- app/src/test/java/ca/antonious/habittracker/habitinteractiontests/CompleteHabitTests.java
+++ app/src/test/java/ca/antonious/habittracker/habitinteractiontests/CompleteHabitTests.java
@@ -3,8 +3,14 @@ package ca.antonious.habittracker.habitinteractiontests;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+
 import ca.antonious.habittracker.habitstorage.IHabitRepository;
 import ca.antonious.habittracker.interactions.CompleteHabit;
+import ca.antonious.habittracker.models.Habit;
+import ca.antonious.habittracker.models.HabitCompletion;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
@@ -16,15 +22,37 @@ public class CompleteHabitTests {
     private IHabitRepository habitRepository;
     private CompleteHabit completeHabit;
 
+    private Habit habitWithoutCompletion;
+    private Habit habitWithCompletion;
+
     @Before
     public void set_up() {
+        habitWithoutCompletion = new Habit();
+        habitWithoutCompletion.setId("Habit-ID");
+        habitWithoutCompletion.setName("AwesomeHabit");
+        habitWithoutCompletion.setStartDate(new Date(1));
+        habitWithoutCompletion.setDaysToComplete(Arrays.asList(Calendar.SUNDAY));
+
+        HabitCompletion habitCompletion = new HabitCompletion(new Date(1));
+        habitCompletion.setId("Completion-ID");
+
+        habitWithCompletion = new Habit();
+        habitWithCompletion.setId("Habit-ID");
+        habitWithCompletion.setName("AwesomeHabit");
+        habitWithCompletion.setStartDate(new Date(1));
+        habitWithCompletion.setDaysToComplete(Arrays.asList(Calendar.SUNDAY));
+        habitWithCompletion.setCompletions(Arrays.asList(habitCompletion));
+
         habitRepository = mock(IHabitRepository.class);
         completeHabit = new CompleteHabit(habitRepository);
     }
 
     @Test
     public void test_complete_ifHabitDoesNotExist_thenNoRepositoryUpdatesOccurs() {
-        fail();
+        when(habitRepository.getHabit("Habit-ID")).thenReturn(null);
+        completeHabit.complete("Habit-ID");
+
+        verify(habitRepository, never()).updateHabit(any(Habit.class));
     }
 
     @Test
